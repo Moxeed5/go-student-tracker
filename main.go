f6691b9acba1f8b7b8f817515250a6b0ee17375d
Max Morgan <maxmmorgan93@gmail.com>
2023-08-31 13:42:57 -0500

    adding comments with general lessons about go + explaining my code for later reference

--- main.go
+++ main.go
@@ -4,26 +4,43 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 	"unicode"
 )
 
+/*important note. When you pass by value, you are createing a copy. When you pass by reference, you are passing the mem address of the object rather than a copy.
+What does this mean?
+It means if I create a classRoom like var newClass ClassRoom and then I use a func that accepts Classroom type as an arg, and I pass in newClass,
+I am passing by value. I am making a copy of newClass. So any modifications that the function does to the ClassRoom arg won't be reflected in my original
+newClass var. And the classRoom arg that I provide only has scope within the function.
+
+So, if I want to modify the original newClass var, I need to pass by reference using a pointer. My func should be modified to accept a ClassRoom pointer rather than
+a ClassRoom data type. you do this by using "*"before the data type. Now there isn't a copy of newClass being made. I am passing the mem address of the newClass
+var that I declared earlier and any manipulations done in the func will be reflected in my newClass var.
+
+Something to consider is that I could also pass by value and set the original var = to the return val of a func. For example newClass = someFunc(newClass)
+Here I am passing by value, and using a copy of the newClass var as the arg for somefunc. I modifying the original newClass var by setting it equal to the
+return val of somefunc(newClass). So I was able to pass by value, and also modify my original var without need to pass by reference. An issue with this is that
+when you are working with huge amounts of data, creating a copy by passing by value is expensive relaitve to passing a mem address with a pointer. These are my
+in the moment thoughts so I'll prob need to update this...*/
+
+//declaring struct types
 type Student struct {
 	firstName string
-	lastName string
+	lastName  string
 }
 
 type AttendenceRecord struct {
-	student Student
+	student   Student
 	isPresent bool
-	Records []time.Weekday
-
+	Records   []time.Weekday
 }
 
 type ClassRoom struct {
-	className string
-	studentList []Student
+	className       string
+	studentList     []Student
 	classAttendence []AttendenceRecord
 }
 
@@ -31,100 +48,108 @@ type School struct {
 	classRoomList []ClassRoom
 }
 
-
-
 func main() {
 	fmt.Println("Welcome to the ClassRoom Manager")
 
-	var school School 
+	var school School
+
+	//for loop and switch statements to manage menu items
 	for {
 		fmt.Println("Please choose from the following: ")
-		fmt.Println("Press 1 to create a new classroom, 2 to add students to a class, 3 take attendence by class and 4 to exit the program.")
-		var menuChoice int
-		fmt.Scanln(&menuChoice)
+		fmt.Println("Press 1 to create a new classroom, 2 to add students to a class, 3 take attendance by class and 4 to exit the program.")
+		menuChoiceStr, err := readLine()
+		if err != nil {
+			fmt.Println("Error reading input. Please try again.")
+			continue
+		}
 
+		menuChoice, err := strconv.Atoi(menuChoiceStr)
+		if err != nil {
+			fmt.Println("Please enter a valid number.")
+			continue
+		}
 		switch menuChoice {
 		case 1:
 			newClass := createClassRoom()
 			school.classRoomList = append(school.classRoomList, newClass)
 
 		case 2:
-			roomSelection := school.selectClassRoom()
-		
+			selection := school.selectClassRoom()
+
 			for {
 				newStu := createStudent()
-				roomSelection.studentList = append(roomSelection.studentList, newStu)
-		
-				var shouldContinue bool
-				for {
-					fmt.Println("Would you like to add another student? (Y/N)")
-					var addAnotherStudent string
-					fmt.Scan(&addAnotherStudent)
-		
-					var err error
-					shouldContinue, err = yesOrNo(addAnotherStudent)
-					if err != nil {
-						fmt.Println(err.Error())
-					} else {
-						break // break out of the inner loop
-					}
+				school.classRoomList[selection].studentList = append(school.classRoomList[selection].studentList, newStu)
+
+				school.listStudentsInClass(selection)
+
+				fmt.Println("Would you like to add another student? (Y/N)")
+				addAnotherStudent, err := readLine()
+				if err != nil {
+					fmt.Println("Error reading input. Please try again.")
+					continue
 				}
-		
+
+				shouldContinue, err := yesOrNo(addAnotherStudent)
+				if err != nil {
+					fmt.Println(err.Error())
+					continue
+				}
+
 				if !shouldContinue {
-					break // break out of the outer loop
+					break
 				}
 			}
-					
-		case 3: 
+			
+
+		case 3:
 			//createStudent()
 		case 4:
 			return
 
-		default: 
+		default:
 			fmt.Println("Invalid choice! Please select from the available options")
 		}
 	}
-	
 }
-
-// when you write a func specifically for a struct, it's a method. Methods syntax is a bit different from a regular func. see below: 
-//the receiver type is School. This func can only be used on the data type School. Classroom is return val. Params would be in the usual place
-
-func (school *School)selectClassRoom() *ClassRoom {
-	
+//prints the classRooms from the classRoom slice in School struct
+func (school *School) selectClassRoom() int {
+	//this loop is similar to foreach in c#. It returns index of each element and every element within a slice or array or map
 	for index, class := range school.classRoomList {
-		fmt.Printf("%d. %s\n", index + 1, class.className)
+		fmt.Printf("%d. %s\n", index+1, class.className)
 	}
-	var selection int
 	for {
-	fmt.Println("Select the class that you would like to add Student to")
-	
-	_, err := fmt.Scan(&selection)
-	flushStdin()
-	 // scan returns the number of items successfully scanned and and error val. I can use _ if I don't care about # of items scanned
-	 if err != nil {
-		fmt.Println("Invalid input, please enter the class number to make a selection")
-		continue //invalid choice was made, continue takes us to the top of the loop which reprompts the user to select a class
-	 } else if selection <= 0 || selection > len(school.classRoomList) {
-		 fmt.Println("Invalid selection")
-		 continue
-	 } else {
-		for index, student := range school.classRoomList[selection-1].studentList{
-			 fmt.Printf("%d. %s %s\n", index+1, student.firstName, student.lastName)
+		fmt.Println("Select the class")
+
+		selectionStr, err := readLine()
+		if err != nil {
+			fmt.Println("Error reading input. Please try again.")
+			continue //if user input is invalid, we skip an interation of this loop and start at the top which would be "Select the Class"
 		}
-		break
-	 }
-	 
+		
+		//readLine returns a string to we import strconv and use func Atoi to convert a string to int
+		//readLine also returns an error and we use if statements to check if error is not nil, if the selection is within the range of our slice..
+		//..and then we return the int selection -1 bc we added 1 to the index value so that it would start at 1 when we printed index to the user in the ..
+		//"foreach" loop
+
+		selection, err := strconv.Atoi(selectionStr) 
+		if err != nil {
+			fmt.Println("Invalid input, please enter the class number to make a selection")
+			continue
+		} else if selection <= 0 || selection > len(school.classRoomList) {
+			fmt.Println("Invalid selection")
+			continue
+		} else {
+			return selection - 1
+		}
 	}
-	
-	return &school.classRoomList[selection-1]
-	
 }
 
-func flushStdin() {
-    var dummy string
-    fmt.Scanln(&dummy)
+func (school *School) listStudentsInClass(selection int) {
+	for index, student := range school.classRoomList[selection].studentList {
+		fmt.Printf("%d. %s %s\n", index+1, student.firstName, student.lastName)
+	}
 }
+
 
 
 func readLine() (string, error) {
@@ -140,8 +165,8 @@ func createClassRoom() ClassRoom {
 		fmt.Println("Enter the name for the classroom: ")
 
 		name, err := readLine()
-		if err != nil || len(name) == 0 {
-			fmt.Println("Error reading input, please try again.")
+		if err != nil {
+			fmt.Println("Error reading input. Please try again.")
 			continue
 		}
 
@@ -155,48 +180,42 @@ func createClassRoom() ClassRoom {
 	return class
 }
 
-
 func createStudent() Student {
 	var student Student
-	var tempFirstName string
-	var tempLastName string
-	
+
 	for {
-	fmt.Printf("Enter first name of Student: \n")
-	fmt.Scan(&tempFirstName)
-	flushStdin()
-	if isValidName(tempFirstName) {
-	student.firstName = tempFirstName
-	break
-	} else {
-		fmt.Printf("%v is not a valid name\n", tempFirstName)
+		fmt.Println("Enter first name of Student:")
+		tempFirstName, err := readLine()
+		if err != nil || !isValidName(tempFirstName) {
+			fmt.Println("Invalid first name. Please try again.")
+			continue
+		}
+		student.firstName = tempFirstName
+		break
 	}
-	}
+
 	for {
-		fmt.Printf("Enter last name of Student: \n")
-		fmt.Scan(&tempLastName)
-		flushStdin()
-		if isValidName(tempLastName){
+		fmt.Println("Enter last name of Student:")
+		tempLastName, err := readLine()
+		if err != nil || !isValidName(tempLastName) {
+			fmt.Println("Invalid last name. Please try again.")
+			continue
+		}
 		student.lastName = tempLastName
 		break
-		} else {
-			fmt.Printf("%v is not a valid name\n", tempLastName)
-		}
 	}
 
 	return student
-
 }
 
 func isValidName(name string) bool {
-    for _, r := range name {
-        if !unicode.IsLetter(r) && !unicode.IsSpace(r) && r != '\'' {
-            return false
-        }
-    }
-    return true
+	for _, r := range name {
+		if !unicode.IsLetter(r) && !unicode.IsSpace(r) && r != '\'' {
+			return false
+		}
+	}
+	return true
 }
-
 
 func yesOrNo(inputString string) (bool, error) {
 	switch strings.ToLower(inputString) {
